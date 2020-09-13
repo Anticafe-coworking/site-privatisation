b0823eb9fb39370a65b8f7aeb8fed06663aa30ed
Boris <boris.pozzo@gmail.com>
2020-09-13 11:45:31 -0400

    Deploy gh-pages

---
+++ _next/static/IVTU18Q6KofMMlmYyx_kK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-7848b25f28ece2b429eb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
